2b78b36a6ae73bc58b2a24d12f3a8e9768925814
Tuan Anh <tuan73176@gmail.com>
2026-01-03 13:42:22 +0000

    sm8550-common: Simplify MSIM scripts
    Since the 4th generation onwards, every Xperia device is capable of
    dual-SIM, no matter what the variant is. Stop setting radio-related
    configuration in MSIM and only use it for detecting variants to set
    the correct NFC configuration.
    
    Change-Id: I01cbd025b0ba453061867c3dcc43327c699112c2

--- rootdir/init.qcom.msim.sh
+++ rootdir/init.qcom.msim.sh
@@ -6,13 +6,6 @@ if [ "$model" = "" ]; then
     model=`grep -aEm1 '(A301SO|SOG10|SOG12|SO-51D|SO-53D))&nbsp;' /dev/block/by-name/LTALabel 2> /dev/null | sed -nE 's/.*((A301SO|SOG10|SOG12|SO-51D|SO-53D))&nbsp;.*/\1/p'`
 fi
 
-case "$model" in
-    "XQ-BC42" | "XQ-BC52" | "XQ-BC62" | "XQ-BC72" | "XQ-BQ42" | "XQ-BQ52" | "XQ-BQ62" | "XQ-BQ72" | "XQ-DQ44" | "XQ-DQ54" | "XQ-DQ62" | "XQ-DQ72" | "XQ-DE44" | "XQ-DE54" | "XQ-DE72" )
-        setprop vendor.radio.hardware.sku ds;;
-    * )
-        setprop vendor.radio.hardware.sku ss;;
-esac
-
 if [ "$model" = "" ]; then
     setprop vendor.radio.ltalabel.model "unknown"
 else
