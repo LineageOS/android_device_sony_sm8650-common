f2fb53aaec26061d48f8d2d58bf795b94d59181e
Igor Belwon <igor.belwon@mentallysanemainliners.org>
2026-03-14 15:56:17 +0100

    sm8650-common: Update common qcom modules list from OnePlus
    Change-Id: I9698bae3762fecf704653ad1ff5f9172f3f8f474

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -100,8 +100,11 @@ TARGET_KERNEL_EXT_MODULES := \
     qcom/opensource/mm-drivers/hw_fence \
     qcom/opensource/mm-drivers/msm_ext_display \
     qcom/opensource/mm-drivers/sync_fence \
+    qcom/opensource/securemsm-kernel \
     qcom/opensource/audio-kernel \
+    qcom/opensource/synx-kernel \
     qcom/opensource/camera-kernel \
+    qcom/opensource/datarmnet-ext/mem \
     qcom/opensource/dataipa/drivers/platform/msm \
     qcom/opensource/datarmnet/core \
     qcom/opensource/datarmnet-ext/aps \
@@ -111,15 +114,20 @@ TARGET_KERNEL_EXT_MODULES := \
     qcom/opensource/datarmnet-ext/perf_tether \
     qcom/opensource/datarmnet-ext/sch \
     qcom/opensource/datarmnet-ext/wlan \
-    qcom/opensource/securemsm-kernel \
     qcom/opensource/display-drivers/msm \
+    qcom/opensource/dsp-kernel \
     qcom/opensource/eva-kernel \
     qcom/opensource/video-driver \
     qcom/opensource/graphics-kernel \
     qcom/opensource/wlan/platform \
     qcom/opensource/wlan/qcacld-3.0/.kiwi_v2 \
+    qcom/opensource/wlan/qcacld-3.0/.qca6750 \
     qcom/opensource/bt-kernel \
-    nxp/opensource/driver \
+    qcom/opensource/spu-kernel \
+    qcom/opensource/mm-sys-kernel/ubwcp \
+    nxp/opensource/driver
+
+TARGET_KERNEL_EXT_MODULES += \
     cirrus/kernel-modules/cs35l45/sound/soc/codecs \
     cirrus/kernel-modules/cs40l25/drivers/misc \
     cirrus/kernel-modules/cs40l25/sound/soc/codecs \
