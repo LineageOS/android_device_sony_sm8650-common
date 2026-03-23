30643e98fd263d572da861b35289cc52e5fb2292
Igor Belwon <igor.belwon@mentallysanemainliners.org>
2026-03-23 14:23:26 +0100

    sm8650-common: fingerprint: Rebrand
    Change-Id: Ica38d07a65fe6b854ebf4fe298021a9ac7435026

--- common.mk
+++ common.mk
@@ -161,7 +161,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint-service.lineage
+    android.hardware.biometrics.fingerprint-service.egis
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
