c99c2e7fdeea5a5c7546447e3fb8e924e25a10b5
Igor Belwon <igor.belwon@mentallysanemainliners.org>
2026-04-03 10:28:56 +0200

    sm8650-common: Drop custom fingerprint HAL
    We can now use the Lineage HAL after patching the FP HAL version.
    
    Change-Id: I574717017bab2d4307aef60608c53d1534e1f193

--- common.mk
+++ common.mk
@@ -159,7 +159,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint-service.egis
+    android.hardware.biometrics.fingerprint-service.lineage
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
